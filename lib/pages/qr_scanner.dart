079661e5d2ed8a3796103d681b5cda2edce076dc
Alejo <agiubel@gmail.com>
2022-10-28 13:30:18 -0400

    feat: toggle flash in qr scanner view

--- lib/pages/qr_scanner.dart
+++ lib/pages/qr_scanner.dart
@@ -7,18 +7,60 @@ class QrScanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    QRViewController? controller;
+    ValueNotifier<bool> flashOn = ValueNotifier<bool>(false);
     return Scaffold(
-      body: QRView(
-        key: qrKey,
-        onQRViewCreated: (qRViewController) {
-          qRViewController.scannedDataStream.listen((event) {
-            if (event.code != null) {
-              qRViewController.stopCamera();
-              qRViewController.dispose();
-              Navigator.pop(context, event.code);
-            }
-          });
-        },
+      body: Stack(
+        children: [
+          QRView(
+            key: qrKey,
+            overlay: QrScannerOverlayShape(
+              borderColor: Colors.red,
+              borderRadius: 10,
+              borderLength: 30,
+              borderWidth: 10,
+              cutOutSize: scanArea,
+            ),
+            onQRViewCreated: (qRViewController) {
+              controller = qRViewController;
+              qRViewController.scannedDataStream.listen((event) {
+                if (event.code != null) {
+                  qRViewController.stopCamera();
+                  qRViewController.dispose();
+                  Navigator.pop(context, event.code);
+                }
+              });
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: IconButton(
+                icon: ValueListenableBuilder(
+                  valueListenable: flashOn,
+                  builder: (context, value, child) => value
+                      ? const Icon(
+                          Icons.flashlight_off,
+                          color: Colors.white,
+                        )
+                      : const Icon(
+                          Icons.flashlight_on,
+                          color: Colors.white,
+                        ),
+                ),
+                onPressed: () async {
+                  await controller?.toggleFlash();
+                  flashOn.value = !flashOn.value;
+                },
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
